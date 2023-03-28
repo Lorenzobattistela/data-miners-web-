73b495badcfd59070a5bf2da712ada27a5fcd96d
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-03-28 07:52:50 -0300

    [style] update newsletter text

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Newsletter = () => {
   return (
-    <div className='w-full py-16 text-white px-4'>
+    <div id='newsletter' className='w-full py-16 text-white px-4'>
       <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
         <div className='lg:col-span-2 my-4'>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>
-            Quer dicas para melhorar o flow do trabalho?
+          <h1 className='md:text-3xl sm:text-3xl text-2xl font-bold py-2'>
+            Quer ser um Beta Tester dos nossos produtos?
           </h1>
-          <p>Inscreva-se na nossa newsletter e receba todas as novidades e dicas.</p>
+          <p>Inscreva o seu email e receba acesso antecipado aos nossos produtos e outras recompensas!</p>
         </div>
         <div className='my-4'>
           <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
